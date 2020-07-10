c2f462110eee5f68254a356b7ecfccc138a8a7c8
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2020-07-11 03:10:50 +0900

    delete debug label

--- exchange_calculator/lib/app.dart
+++ exchange_calculator/lib/app.dart
@@ -8,6 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       navigatorKey: context.watch(),
       home: CalculatorPage(),
     );
