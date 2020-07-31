baaaae50e8e27f7ea51fb4dcd7e0e1c1e33a40f5
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2020-07-31 21:11:43 +0900

    analytics, crashlytics関連の追加

--- exchange_calculator/lib/main.dart
+++ exchange_calculator/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:exchangecalculator/models/calculator_logic.dart';
@@ -5,13 +8,20 @@ import 'package:exchangecalculator/Util/neumorphic_theme.dart';
 
 import 'app.dart';
 
-void main() => runApp(
-  MultiProvider(
-    providers: [
-      Provider(create: (context) => GlobalKey<NavigatorState>()),
-      ProxyProvider0<NeumorphicTheme>(update: (context,_) => lightNeumorphicTheme),
-      ChangeNotifierProvider<Calculator>(create: (context) => Calculator())
-    ],
-    child: const App(),
-  ),
-);
+void main() {
+  Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+  runZoned(() {
+    runApp(
+      MultiProvider(
+        providers: [
+          Provider(create: (context) => GlobalKey<NavigatorState>()),
+          ProxyProvider0<NeumorphicTheme>(update: (context,_) => lightNeumorphicTheme),
+          ChangeNotifierProvider<Calculator>(create: (context) => Calculator())
+        ],
+        child: const App(),
+      ),
+    );
+  }, onError: Crashlytics.instance.recordError);
+}
